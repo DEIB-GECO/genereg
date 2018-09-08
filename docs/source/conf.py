f0046ca6ae2d006edd7c7b0ef5b466418e58107a
Kia23 <kia.open18@gmail.com>
2018-09-08 17:26:38 +0200

    add mock

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,16 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['pandas', 'numpy', 'openpyxl', 'math', 'pickle', 'xlsxwriter', 'collections', 'sklearn', 'statsmodels', 'os', 'urllib', 'mlxtend']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 
 # -- Project information -----------------------------------------------------
 
